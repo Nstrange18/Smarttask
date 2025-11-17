5d899f86bfbbb17cfbe1eb954dc1f6651a35ee08
Allen Nwuba <nwubachukwuemelie@gmail.com>
2025-11-17 15:49:35 +0100

    s

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://smarttask-eight.vercel.app"],
+    origin: ["http://localhost:5173", "https://smarttask-nu.vercel.app"],
     credentials: true,
   })
 );
