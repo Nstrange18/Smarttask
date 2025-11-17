3126b7e2be7dd3ea165aef7d363e22750e4a4289
Allen Nwuba <nwubachukwuemelie@gmail.com>
2025-11-17 15:21:28 +0100

    Deplyed on render

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://smarttask-gnhy.onrender.com",
 });
 
 // REQUEST INTERCEPTOR → attach token
